1fbeede9b32cd121e31677fef044a844196b0ef6
ualali <74206178+ualali@users.noreply.github.com>
2021-09-22 15:34:00 -0500

    Create Location Consumer module

---
+++ modules/location-consumer/app/udaconnect/consumer.py
@@ -0,0 +1,50 @@
+from kafka import KafkaConsumer
+import os
+import logging
+import json
+import psycopg2
+
+
+DB_USERNAME = os.environ["DB_USERNAME"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_NAME = os.environ["DB_NAME"]
+
+
+consumer = KafkaConsumer(topics="Locations", bootstrap_servers="udaconnect-kafka:9092")
+
+
+def save_location(location):
+    connection = psycopg2.connect(
+        dbname=DB_NAME,
+        port=DB_PORT,
+        user=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+    )
+    query = "INSERT INTO location (person_id, coordinate) VALUES ({}, ST_Point({}, {}));".format(
+        int(location["person_id"]),
+        float(location["latitude"]),
+        float(location["longitude"]),
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def consume():
+    for message in consumer:
+        print("Location received: {}".format(message))
+
+        decoded_message = message.value.decode("utf-8")
+        location = json.loads(message)
+
+        save_location(location)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
